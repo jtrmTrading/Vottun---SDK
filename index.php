ccd11b6562af35892f4894720f94c730eec2b101
jtrmTrading <162365021+jtrmTrading@users.noreply.github.com>
2024-04-24 12:28:35 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>PROYECTO SDK</title>
+  <p></p>
+ </head>
+ <body>
+<?php
+require_once 'sdkVottun.php';
+?>
+</body>
+</html>
